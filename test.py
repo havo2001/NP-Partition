03eacfb9df177207825b9f0a7f5ea2198e3e2c31
thuha <thuhavothi2001@gmail.com>
2022-05-19 17:06:11 +0300

    Update test

--- test.py
+++ test.py
@@ -8,20 +8,17 @@ from main import TopDownDynamicSolution
 
 class Test(unittest.TestCase):
     def test_case1(self):
-        random_list = []
-        for i in range(10):
-            n = random.randint(1, 10)
-            random_list.append(n)
+        random_list = [random.randint(1, 100) for i in range(1000)]
         brute_force = BruteForceSolution(random_list)
         top_down = TopDownDynamicSolution(random_list)
         bottom_up = BottomUpDynamicSolution(random_list)
         self.assertEqual(brute_force.can_partition(), top_down.can_partition(), bottom_up.can_partition())
 
     def test_case2(self):
-        new_list = [2, 3, 2, 1, 4]
-        brute_force = BruteForceSolution(new_list)
-        top_down = TopDownDynamicSolution(new_list)
-        bottom_up = BottomUpDynamicSolution(new_list)
+        random_list = [i for i in range(1000)]
+        brute_force = BruteForceSolution(random_list)
+        top_down = TopDownDynamicSolution(random_list)
+        bottom_up = BottomUpDynamicSolution(random_list)
         self.assertEqual(brute_force.can_partition(), True)
         self.assertEqual(top_down.can_partition(), True)
         self.assertEqual(bottom_up.can_partition(), True)
@@ -35,14 +32,12 @@ class Test(unittest.TestCase):
         self.assertEqual(top_down.can_partition(), True)
         self.assertEqual(bottom_up.can_partition(), True)
 
-
-
-
-
-
-
-
-
-
-
+    def test_case4(self):
+        random_list = [1, 5, 6, 3, 3]
+        brute_force = BruteForceSolution(random_list)
+        top_down = TopDownDynamicSolution(random_list)
+        bottom_up = BottomUpDynamicSolution(random_list)
+        self.assertEqual(brute_force.can_partition(), True)
+        self.assertEqual(top_down.can_partition(), True)
+        self.assertEqual(bottom_up.can_partition(), True)
 
